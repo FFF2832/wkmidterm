4582db47b6aa6ccf8b9d009a1c90cd0bf34423e1
fiona <fiona.w32@gmail.com>
2022-04-09 15:53:18 +0800

    second commit

--- App.js
+++ App.js
@@ -1,44 +1,18 @@
-import React, { useState } from "react";
-import { NativeBaseProvider, Center, HStack, Button, Text, Switch } from "native-base";
+import 'react-native-gesture-handler';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
+
+import Navigation from './src/navigation';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [colorMode, setColorMode] = useState("light");
-  const toggleColorMode = () => {
-    if (colorMode == "light") setColorMode("dark");
-    else setColorMode("light")
-  }
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bg={colorMode == "light" ? "white" : "black"}>
-        <HStack space={20}>
-          <Button borderRadius={0} width={70} onPress={() => setCount(count + 1)}>
-            <Text fontSize={40} color={colorMode == "light" ? "white" : "black"}>+</Text>
-          </Button>
-          <Button borderRadius={0} width={70} onPress={() => setCount(count - 1)}>
-            <Text fontSize={40} color={colorMode == "light" ? "white" : "black"}>-</Text>
-          </Button>
-        </HStack>
-        <Text fontSize={40} mt={20} color={colorMode == "dark" ? "white" : "black"}>
-          {count}
-        </Text>
-        <HStack mt={20} space={8} alignItems="center" >
-          <Text fontSize="lg" color={colorMode == "dark" ? "white" : "black"}>
-            {colorMode == "light" ? "Light Mode" : "Dark Mode"}
-          </Text>
-          <Switch
-            name="light Mode"
-            isChecked={colorMode === "light"}
-            onToggle={toggleColorMode}
-            accessibilityLabel="display-mode"
-            accessibilityHint="light or dark mode"
-          />
-        </HStack>
-
-      </Center>
-    </NativeBaseProvider>
+    <SafeAreaProvider>
+      <NativeBaseProvider>
+        <Navigation />
+      </NativeBaseProvider>
+    </SafeAreaProvider>
   );
-};
-
+}
 
 export default App;
