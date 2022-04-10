bc20157232d91fc840e5fcc62571e121090b49bb
fiona <fiona.w32@gmail.com>
2022-04-10 18:17:04 +0800

    setting page

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, HStack, Pressable } from "native-base";
 import AntDesign from "react-native-vector-icons/AntDesign";
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const ListItem = ({ title, navigation, destination }) => {
   return (
     <Pressable
@@ -15,14 +15,15 @@ const ListItem = ({ title, navigation, destination }) => {
           borderColor: "blueGray.500",
           borderButtomWidth: 0.6,
         }}
-        _light={{ bg: "white" }}
+        _light={{ bg: "#E2D5C3" }}
         px="4"
         py="4"
-        borderTopWidth={1}
+        borderTopWidth={0}
         borderColor="lightgray"
         justifyContent="space-between"
       >
-        <Text fontSize={16}>{title}</Text>
+         <MaterialCommunityIcons name="cog-outline" color="#4F5B57" bg="#4F5B57" size={26} />
+        <Text fontSize={15}>{title}</Text>
         <AntDesign name="right" color="gray" size={16} />
       </HStack>
     </Pressable>
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "native-base";
+import { Box ,Image,Center,Text,HStack} from "native-base";
 import ListItem from "../components/ListItem"
 
 const SettingsScreen = ({ navigation }) => {
@@ -7,14 +7,42 @@ const SettingsScreen = ({ navigation }) => {
         <Box 
             flex={1}
             _dark={{ bg: "blueGray.900" }}
-            _light={{ bg: "blueGray.50" }}        
+            _light={{ bg: "#E2D5C3" }}        
         >
+            <Center>
+                <Image
+                 margin="1"
+                 width="140"
+                 height="140"
+                 padding="2"
+                 borderRadius="7"
+                 paddingRight={4}
+            //   flex="1"
+            //   justifyContent="center"
+                marginTop={19}
+              source={{ uri: "https://github.com/FFF2832/wkmidterm/blob/master/src/images/Group%2054.png?raw=true" }}
+              alt="artist"
+            />
+            <Text color="#4F5B57" fontSize={20} fontWeight="bold">Jennifer Lin</Text>
+            <HStack>
+                <Text color="#4F5B57" fontSize={12} fontWeight="bold" marginRight={3}>已收藏次數</Text>
+                <Text color="#4F5B57" fontSize={12} fontWeight="bold"  marginRight={3}>已寫文章數</Text>
+                <Text color="#4F5B57" fontSize={12} fontWeight="bold"  marginRight={2}>已加入時間</Text>
+            </HStack>
+            <HStack>
+                <Text color="#4F5B57" fontSize={12} fontWeight="bold" marginRight={20}>5</Text>
+                <Text color="#4F5B57" fontSize={12} fontWeight="bold"  marginRight={20}>5</Text>
+                <Text color="#4F5B57" fontSize={12} fontWeight="bold"  marginRight={13}>5</Text>
+            </HStack>
+            </Center>
+
             <Box 
-                mt="1" borderBottomWidth={1} borderColor="lightgray"
+                mt="12" borderBottomWidth={1} borderColor="lightgray"
                 _dark={{ borderColor: 'blueGray.500', borderButtomWidth: 0.6 }}
             >
-                <ListItem title="Display" navigation={navigation} destination="DisplaySetting" />
-                <ListItem title="Account" navigation={navigation} destination="AccountSetting"/>
+
+                <ListItem title="主題設定"   navigation={navigation} destination="DisplaySetting" />
+                {/* <ListItem title="Account" navigation={navigation} destination="AccountSetting"/> */}
             </Box>            
         </Box>
 
