3e840baed3792054374452fb076107c638534bbc
fiona <fiona.w32@gmail.com>
2022-04-13 15:46:35 +0800

    albumscreen
    Co-authored-by: zhiyu414 <zhiyu414@users.noreply.github.com>

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { FlatList ,Box} from "native-base";
+import { FlatList ,Box,Center} from "native-base";
 import AlbumDetail from "./AlbumDetail";
 
 const AlbumList = ({ list, navigation }) => {
   const renderItem = ({ item }) => <AlbumDetail album={item} navigation={navigation} />;
   return (
+
     <FlatList
     _dark={{ bg: "#4F5B57" }}
     _light={{ bg: "#E2D5C3" }}
